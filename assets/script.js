b3d052df67d890a91f2db3870772e14e43c30497
GIR <gireches@gmail.com>
2021-01-24 19:09:30 -0800

    completed functionality

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     let getDate = function(days){
         let someDate = new Date();
         let DaysToAdd = days; 
-        someDate.setDate(someDate.getDate() + DaysToAdd);
+        someDate.setDate(someDate.getDate()  + DaysToAdd);
 
         let dd = someDate.getDate();
         let mm = someDate.getMonth() + 1;
@@ -14,6 +14,9 @@ $(document).ready(function(){
     }
 
     // wind speed
+    let mph = (speed) => {
+        return parseFloat(speed * (3600/1609.344)).toFixed(2);
+    }
 
     // searched cities
     let searchedCities = [];
@@ -24,6 +27,7 @@ $(document).ready(function(){
     // last searched city
 
     // city id seperated for new call of city change
+    const apiKey = "26fbb33b8a517b0acbc32b21c9e60038";
 
     // weather function
     let weatherFinder = function(cityName, searched){
@@ -35,17 +39,73 @@ $(document).ready(function(){
         // first call gets city name
         $.ajax({url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`, success: function(result){
         
-        // if request was sent from searchfield
+        // if request was sent from search field
         if(searched === true){
-            // if value was sent from searchfield form
-            
+            // if value was sent from search field form
+            if(searchedCities.includes($('#search input').val()) != true){
+                searchedCities.push($('#search input').val());
+                localStorage.setItem('citysearch', JSON.stringify(searchedCities));
+            }
+            localStorage.setItem('lastCitySearch', $('#search input').val());
         }
+
+        // sets search field presets
+        Array.from(searchedCities).forEach(check => {
+            $('#search datalist').append(`<option value='${check}'></option>`);
+        })
+
+            cityId = result.id;
+            // second call uses result.id data from the first to display weather and to pass the third for the uv
+            $.ajax({url: `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${apiKey}&units=imperial`, success: function(result){
+                // empties current day
+                $('#currentDay').html('');
+                // sets current day information
+                $('#currentDay').append(`<div class='blockHeading'><h2>${result.city.name} (${getDate(0)})</h2><img src='https://openweathermap.org/img/w/${result.list[0].weather[0].icon}.png' alt="${result.list[0].weather[0].description}" width='50' height='50'>`);
+                // sets temp
+                $("#currentDay").append(`<p class="temperature">Temperature: ${result.list[0].main.temp} °F</p>`);
+                // sets humidity
+                $("#currentDay").append(`<p class="humidity"> Humidity: ${result.list[0].main.humidity} %</p>`);
+                // sets wind
+                $("#currentDay").append(`<p class="wind_speed">Wind Speed: ${mph(result.list[0].wind.speed)} MPH</p>`);
+
+                // third call uses coordinate data from second call to call uv data
+                $.ajax({url: `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=` + result.city.coord.lat + "&lon=" + result.city.coord.lon,
+                success: function(result){
+                    $("#currentDay").append(`<p class="uv">UV Index: <span>${result.value}</span></p>` );
+                    // Call ends re enable api calls
+                    $('button').removeClass('wait');
+                    $('button').attr('disabled', false);
+                }})
+
+                // 5 day forecast
+                // clears forecast data
+                $('#forecast .days').html('');
+                // append forecast data
+                for(let i=1; i <= 5; i++){
+                    let forecastBlock = function(i){
+                        return('<div>' + '<p class="date">' + getDate(i) + '</p>' + `<img src="https://openweathermap.org/img/w/${result.list[i].weather[0].icon}.png" alt="${result.list[i].weather[0].description}" width='50' height='50'>` + `<p class="temperature">Temp: ${result.list[i].main.temp}&nbsp;°F</p>` +
+                        `<p class="humidity">Humidity: ${result.list[i].main.humidity}&nbsp;%"</p>` +
+                        '</div>');
+                    }
+
+                    $('#forecast .days').append(forecastBlock(i));
+                }
+            }});
         }})
     }
 
+    if( localStorage.getItem("lastCitySearch")){
+        weatherFinder( localStorage.getItem("lastCitySearch"), false);
+      }else{
+        weatherFinder("Atlanta", false);
+      }
+    
+      $("#presetCities button").on( "click", function() {
+        weatherFinder($(this).html().toString(), false);
+      });
+    
+      $("#search button").on( "click", function() {
+        weatherFinder($("#search input").val(), true);     
+      });
 
-
-
-
-
-});    
+});            
